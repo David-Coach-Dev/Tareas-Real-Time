8fafc9ffa03950a417141549b47d37692932f72d
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2022-12-29 23:42:48 -0500

    act arch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import app from './app';
 import { Server as websocketServer } from 'socket.io';
 import http from 'http';
+import { connectDB } from './db';
+
+connectDB();
 
 const server = http.createServer(app);
 const httpServer = server.listen(3000);
