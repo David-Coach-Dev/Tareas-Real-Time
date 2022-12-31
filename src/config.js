557cd0b47b0c85369f6de5d05f8edfef5bb118f6
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2022-12-31 01:47:11 -0500

    act

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 import { config } from 'dotenv';
 config();
-export const MONGODB_URI = process.env.MONGODB_URI;
+export const MONGODB_URI = process.env.MONGODB_URI_ATLAS || process.env.MONGODB_URI_LOCAL;
 export const PORT = process.env.PORT || 3000;
